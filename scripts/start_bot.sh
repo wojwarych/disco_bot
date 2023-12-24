cfeb2a14b27e0748624a4ca99048829c35f93dd4
wojwarych <woj.warych@gmail.com>
2023-12-24 13:09:50 +0100

    feat(deployment): Create dockerized version

--- scripts/start_bot.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/ubuntu/disco_bot
-
-.venv/bin/activate
-
-python src/main.py
--- scripts/stop_bot.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-PROC=$(pgrep -f 'python src/main.py')
-
-if [ ! -z "$PROC" ]
-then
-  kill ${PROC}
-fi
