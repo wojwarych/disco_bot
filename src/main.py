10ea31f59bac2314b609a4c61d430a08a2f2219d
wojwarych <woj.warych@gmail.com>
2024-06-25 15:04:22 +0200

    feat(barka): Implement basic papaj on 21:37 in PL

--- src/main.py
+++ src/main.py
@@ -1,31 +1,57 @@
-import os
+import asyncio
 import logging
+import logging.handlers
+import os
+import sys
 
 import discord
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 
 from msg_builder import MsgBuilder
+from scheduler import PapajScheduler
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+BARKA_CHANNEL_ID = int(os.getenv("BARKA_CHANNEL_ID", ""))
 
 
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("discord")
+logger.setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename="discord.log",
+    encoding="utf-8",
+    maxBytes=32 * 1024 * 1024,  # 32MiB
+    backupCount=5,
+)
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_format = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+stream_handler.setFormatter(stream_format)
+logger.addHandler(handler)
+logger.addHandler(stream_handler)
+
 bot = Bot(command_prefix="$", intents=discord.Intents.all())
 
 
 @bot.event
 async def on_ready():
-    logger.debug("Connected!")
+    logger.info("Connected!")
 
 
-@bot.command()
+@bot.command(name="bless")
 async def bless(ctx):
     msg_builder = MsgBuilder(message=ctx.message)
     await ctx.send(msg_builder.bless())
 
 
+async def main(bot):
+    async with bot:
+        await bot.add_cog(PapajScheduler(bot, BARKA_CHANNEL_ID))
+        await bot.start(TOKEN)
+
+
 if __name__ == "__main__":
-    bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
+    asyncio.run(main(bot))
---
+++ src/scheduler/scheduler.py
@@ -0,0 +1,47 @@
+import asyncio
+import datetime
+import logging
+import logging.handlers
+import os
+import sys
+from zoneinfo import ZoneInfo
+
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot
+from dotenv import load_dotenv
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_format = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+stream_handler.setFormatter(stream_format)
+logger.addHandler(stream_handler)
+
+load_dotenv()
+BARKA_HOUR = int(os.getenv("BARKA_HOUR", 14))
+BARKA_MINUTE = int(os.getenv("BARKA_MINUTE", 37))
+
+time = datetime.time(
+    hour=BARKA_HOUR, minute=BARKA_MINUTE, tzinfo=ZoneInfo("Europe/Warsaw")
+)
+
+
+class PapajScheduler(commands.Cog):
+    def __init__(self, bot: Bot, channel_id: int) -> None:
+        self.bot = bot
+        self.channel_id = channel_id
+        self.papaj_task.start()
+
+    def cog_unload(self):
+        self.papaj_task.cancel()
+
+    @tasks.loop(time=time)
+    async def papaj_task(self):
+        message_channel = self.bot.get_channel(self.channel_id)
+        with open("./src/papaj.txt", "r") as f:
+            for line in f:
+                await message_channel.send(line)
+                await asyncio.sleep(3)
